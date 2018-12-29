eec53c92852fc9f8f0df4b4dfbef45ec3ab5cd16
Leonardo Medeiros <>
2018-12-29 14:44:03 -0200

    - added some corrections on commentary

--- udemy/estatistica/05 - Distribuição de Poisson/distribuicao_poisson.py
+++ udemy/estatistica/05 - Distribuição de Poisson/distribuicao_poisson.py
@@ -4,6 +4,8 @@
 from scipy.stats import poisson
 
 """
+P(X = x)
+
 Exemplo 1:
     O número de acidente de carros que ocorrem por dia é 2.
     Qual a probabilidade de ocorrerem 3 acidentes no dia?
@@ -12,17 +14,21 @@ Exemplo 1:
 resposta_exemplo_1 = poisson.pmf(3, 2)
 
 """
+P(X < x)
+
 Exemplo 2:
     O número de acidente de carros que ocorrem por dia é 2.
-    Qual a probabilidade de ocorrerem 3 ou menos acidentes no dia?
+    Qual a probabilidade de ocorrerem 3 ou MENOS acidentes no dia?
 """
 
 resposta_exemplo_2 = poisson.cdf(3, 2)
 
 """
+P(X > x)
+
 Exemplo 3:
     O número de acidente de carros que ocorrem por dia é 2.
-    Qual a probabilidade de ocorrerem mais de 3 acidentes no dia?
+    Qual a probabilidade de ocorrerem MAIS de 3 acidentes no dia?
 """
 
 resposta_exemplo_3 = poisson.sf(3, 2)
