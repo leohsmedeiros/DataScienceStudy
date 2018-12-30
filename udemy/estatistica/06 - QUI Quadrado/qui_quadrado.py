012a823244158c78953590ac39d8e5f9d8388f3b
Leonardo Medeiros <>
2018-12-30 14:50:54 -0200

    - added amostragem simples, centralidade, variabilidade, t_student..

--- udemy/estatistica/06 - QUI Quadrado/qui_quadrado.py
+++ udemy/estatistica/06 - QUI Quadrado/qui_quadrado.py
@@ -19,7 +19,7 @@ novela = np.array([[19, 6], [43, 32]])
 contingency = chi2_contingency(novela)
 """
 Palavras do professor:
-    Oque mais interessa, neste caso, é o segundo parâmetro, que é o valor de P.
+    Oque mais interessa, neste caso, é o segundo parâmetro, que é o valor de P (que no caso é 0.1534...).
     Então como este valor, é maior que o alpha de 0.05 então quer dizer que nós não podemos
     rejeitar a hipótese nula. Neste caso, não há diferença significativa, além do acaso.
 """
